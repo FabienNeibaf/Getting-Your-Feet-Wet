89efcb8c3ef0ecf9cd2b24665e84fd387c9523fd
Fabien RAKOTOMAMPIANDRA <fabienlintone@yahoo.fr>
2019-04-29 16:39:57 +0300

    Finish toy app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "Hello World! I'm Fabien"
+    render html: "Hello World"
   end
 end
